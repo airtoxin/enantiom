fa51e61255e272040ad709ee17c7a911bb968684
airtoxin <airtoxin@icloud.com>
2021-08-28 20:08:45 +0900

    Add arg parse

--- packages/enantiom-cli/src/index.ts
+++ packages/enantiom-cli/src/index.ts
@@ -1,20 +1,13 @@
 import { parse } from "ts-command-line-args";
 
-interface ICopyFilesArguments {
-  sourcePath: string;
-  targetPath: string;
-  copyFiles: boolean;
-  resetPermissions: boolean;
-  filter?: string;
-  excludePaths?: string[];
+interface EnantiomCliArgument {
+  "artifact-path"?: string;
+  config?: string;
 }
 
-// args typed as ICopyFilesArguments
-export const args = parse<ICopyFilesArguments>({
-  sourcePath: String,
-  targetPath: String,
-  copyFiles: { type: Boolean, alias: "c" },
-  resetPermissions: Boolean,
-  filter: { type: String, optional: true },
-  excludePaths: { type: String, multiple: true, optional: true },
+export const args = parse<EnantiomCliArgument>({
+  "artifact-path": { type: String, optional: true, alias: "a" },
+  config: { type: String, optional: true, alias: "c" },
 });
+
+console.log("@args", args);
