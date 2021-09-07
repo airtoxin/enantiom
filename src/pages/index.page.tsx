34e4697c759e94de1f777f171dc63dba75386929
airtoxin <airtoxin@icloud.com>
2021-09-08 02:04:47 +0900

    Add statistics

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,6 @@
-import { VoidFunctionComponent } from "react";
+import { useMemo, VoidFunctionComponent } from "react";
 import { GetStaticProps } from "next";
-import { Layout } from "antd";
+import { Layout, Statistic, Card, Row, Col } from "antd";
 import { resolve } from "path";
 import { promises as fs } from "fs";
 import { AppLayout } from "./AppLayout";
@@ -10,6 +10,15 @@ import Head from "next/head";
 const { Content } = Layout;
 
 const HomePage: VoidFunctionComponent<{ state: State }> = ({ state }) => {
+  const counts = useMemo(() => {
+    const total = state.results.length;
+    const success = state.results.filter((r) =>
+      r.screenshots.every((s) => s.diff == null)
+    ).length;
+    const failure = total - success;
+    return { total, success, failure };
+  }, [state.results]);
+
   return (
     <AppLayout state={state}>
       <Head>
@@ -23,7 +32,33 @@ const HomePage: VoidFunctionComponent<{ state: State }> = ({ state }) => {
           margin: 0,
           minHeight: 280,
         }}
-      />
+      >
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card>
+              <Statistic title="Total results" value={counts.total} />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Statistic
+                title="Success results"
+                value={counts.success}
+                valueStyle={{ color: "#3f8600" }}
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Statistic
+                title="Failure results"
+                value={counts.failure}
+                valueStyle={{ color: "#cf1322" }}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Content>
     </AppLayout>
   );
 };
